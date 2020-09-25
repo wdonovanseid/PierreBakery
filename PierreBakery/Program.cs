d54c55def8ccbc36c9f5b60fb9630b646614bc6b
William Donovan-Seid <wdstwo@live.com>
2020-09-25 13:16:20 -0700

    refactor Main() to use method calls instead of restarting programming whenever user enters invalid number

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -12,19 +12,8 @@ namespace PierreBakery
       Console.WriteLine("All Bread: Buy 2, get 1 free. A single loaf costs $5.");
       Console.WriteLine("All Pastries: Buy 1 for $2 or 3 for $5.");
       Console.WriteLine("The Fabled BreadStick: Buy 1 for $100, 3 for $75 each or 10 for $50 each.");
-      Console.WriteLine("How many Baguettes would you like?");
-      string strBaguette = Console.ReadLine();
+      int numBaguette = GetBaguettes();
       int value;
-      int numBaguette = 0;
-      if (int.TryParse(strBaguette, out value) && int.Parse(strBaguette) >= 0)
-      {
-        numBaguette = int.Parse(strBaguette);
-      }
-      else
-      {
-        Console.WriteLine("Please enter a valid number.");
-        Main();
-      }
       Console.WriteLine("How much Sourdough Bread would you like?");
       string strSourdough = Console.ReadLine();
       int numSourdough = 0;
@@ -77,6 +66,21 @@ namespace PierreBakery
       CustomerOrder cusOrder = new CustomerOrder(numBread, numPastry, numFable);
       Console.WriteLine("The total cost of your order is $" + cusOrder._totalOrderCost);
       Console.WriteLine("Buy more?");
+    }
+    public static int GetBaguettes()
+    {
+      Console.WriteLine("How many Baguettes would you like?");
+      string strBaguette = Console.ReadLine();
+      int value;
+      if (int.TryParse(strBaguette, out value) && int.Parse(strBaguette) >= 0)
+      {
+        return int.Parse(strBaguette);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid number.");
+        return GetBaguettes();
+      }
     }
   }
 }
