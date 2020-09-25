022069b0f4ea2af44e1e3bc23a82cd287833522b
William Donovan-Seid <wdstwo@live.com>
2020-09-25 09:22:05 -0700

    add in different Bread types (all priced same though)

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -9,11 +9,18 @@ namespace PierreBakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("Here is the costs of our products today:");
-      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
-      Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
-      Console.WriteLine("How much Bread would you like?");
-      string strBread = Console.ReadLine();
-      int numBread = int.Parse(strBread);
+      Console.WriteLine("All Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("All Pastries: Buy 1 for $2 or 3 for $5.");
+      Console.WriteLine("How many Baguettes would you like?");
+      string strBaguette = Console.ReadLine();
+      int numBaguette = int.Parse(strBaguette);
+      Console.WriteLine("How much Sourdough Bread would you like?");
+      string strSourdough = Console.ReadLine();
+      int numSourdough = int.Parse(strSourdough);
+      Console.WriteLine("How much Rye Bread would you like?");
+      string strRye = Console.ReadLine();
+      int numRye = int.Parse(strRye);
+      int numBread = numBaguette + numSourdough + numRye;
       Console.WriteLine("How many Pastries would you like?");
       string strPastry = Console.ReadLine();
       int numPastry = int.Parse(strPastry);
