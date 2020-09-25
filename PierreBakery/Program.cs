cda4bdc0f090d4fbfc3514eb9cb3d6a266298931
William Donovan-Seid <wdstwo@live.com>
2020-09-25 10:59:11 -0700

    add in if else statements to restart program if user doesn't input a valid number

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -14,28 +14,66 @@ namespace PierreBakery
       Console.WriteLine("The Fabled BreadStick: Buy 1 for $100, 3 for $75 each or 10 for $50 each.");
       Console.WriteLine("How many Baguettes would you like?");
       string strBaguette = Console.ReadLine();
-      if (!(int.Parse(strBaguette)) || int.Parse(strBaguette) < 0)
+      int value;
+      int numBaguette = 0;
+      if (int.TryParse(strBaguette, out value) && int.Parse(strBaguette) >= 0)
       {
-        Console.WriteLine("Please enter a valid number.");
+        numBaguette = int.Parse(strBaguette);
       }
       else
       {
-
+        Console.WriteLine("Please enter a valid number.");
+        Main();
       }
-      int numBaguette = int.Parse(strBaguette);
       Console.WriteLine("How much Sourdough Bread would you like?");
       string strSourdough = Console.ReadLine();
-      int numSourdough = int.Parse(strSourdough);
+      int numSourdough = 0;
+      if (int.TryParse(strSourdough, out value) && int.Parse(strSourdough) >= 0)
+      {
+        numSourdough = int.Parse(strSourdough);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid number.");
+        Main();
+      }
       Console.WriteLine("How much Rye Bread would you like?");
       string strRye = Console.ReadLine();
-      int numRye = int.Parse(strRye);
+      int numRye = 0;
+      if (int.TryParse(strRye, out value) && int.Parse(strRye) >= 0)
+      {
+        numRye = int.Parse(strRye);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid number.");
+        Main();
+      }
       int numBread = numBaguette + numSourdough + numRye;
       Console.WriteLine("How many Pastries would you like?");
       string strPastry = Console.ReadLine();
-      int numPastry = int.Parse(strPastry);
+      int numPastry = 0;
+      if (int.TryParse(strPastry, out value) && int.Parse(strPastry) >= 0)
+      {
+        numPastry = int.Parse(strPastry);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid number.");
+        Main();
+      }
       Console.WriteLine("How many Fabled Breadsticks would you like?");
       string strFable = Console.ReadLine();
-      int numFable = int.Parse(strFable);
+      int numFable = 0;
+      if (int.TryParse(strFable, out value) && int.Parse(strFable) >= 0)
+      {
+        numFable = int.Parse(strFable);
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid number.");
+        Main();
+      }
       CustomerOrder cusOrder = new CustomerOrder(numBread, numPastry, numFable);
       Console.WriteLine("The total cost of your order is $" + cusOrder._totalOrderCost);
     }
