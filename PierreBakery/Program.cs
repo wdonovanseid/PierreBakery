95309a7ad4e5547876b3049d5ee2c8609cb8d742
William Donovan-Seid <wdstwo@live.com>
2020-09-25 08:35:46 -0700

    add basic Program.cs layout of project

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -4,7 +4,18 @@ namespace PierreBakery
   {
     public static void Main()
     {
-      Console.WriteLine("");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Here is the costs of our products today:");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
+      Console.WriteLine("How much Bread would you like?");
+      string strBread = Console.ReadLine();
+      int numBread = int.Parse(strBread);
+      Console.WriteLine("How many Pastries would you like?");
+      string strPastry = Console.ReadLine();
+      int numPastry = int.Parse(numBread);
+      int totalCost = Something(numBread, numPastry);
+      Console.WriteLine("The total cost of your order is $" + totalCost);
     }
   }
 }
