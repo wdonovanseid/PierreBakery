b9fda54701d39aa29c8b1191d48b502a241a81e3
William Donovan-Seid <wdstwo@live.com>
2020-09-25 08:44:18 -0700

    add methods to output costs of bread and pastries

--- PierreBakery/Models/Stock.cs
+++ PierreBakery/Models/Stock.cs
@@ -4,10 +4,16 @@ namespace Stock
 {
   public class Bread
   {
-
+    public int BreadCost(int numBread)
+    {
+      return ((numBread / 2) + (numBread % 2)) * 5;
+    }
   }
   public class Pastry
   {
-    
+    public int PastryCost(int numPastry)
+    {
+      return (numPastry / 3) * 5 + (numPastry % 3) * 2;
+    }
   }
 }
