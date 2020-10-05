8b25cf9076e78eb5b5613bf791ed735507606780
William Donovan-Seid <wdstwo@live.com>
2020-10-04 18:45:54 -0700

    add tests for simpler behaviors of CustomerOrder class constructor

--- PierreBakery.Tests/ModelTests/StockTests.cs
+++ PierreBakery.Tests/ModelTests/StockTests.cs
@@ -6,8 +6,32 @@ namespace PierreBakery.Tests
   [TestClass]
   public class PierreBakeryTests
   {
+
     [TestMethod]
-    public void BreadCost_GetCorrectCostOfBread_Int()
+    public void CustomerOrderConstructor_MakesCustomerOrderInstance_CustomerOrder()
+    {
+      CustomerOrder testCustomerOrder = new CustomerOrder(1,2,3);
+      Assert.AreEqual(typeof(CustomerOrder), testCustomerOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetBreadOrder_ReturnsBreadOrder_Int()
+    {
+      CustomerOrder testCustomerOrder = new CustomerOrder(1,2,3);
+      int result = testCustomerOrder._breadOrder;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void BreadCost_GetCorrectCostOfOneBread_Int()
+    {
+      int testBread = 1;
+      int testBreadCost = Bread.BreadCost(testBread);
+      Assert.AreEqual(5, testBreadCost);
+    }
+
+    [TestMethod]
+    public void BreadCost_GetCorrectCostOfMultipleBread_Int()
     {
       int testBread = 4;
       int testBreadCost = Bread.BreadCost(testBread);
@@ -15,7 +39,15 @@ namespace PierreBakery.Tests
     }
 
     [TestMethod]
-    public void PastryCost_GetCorrectCostOfPastry_Int()
+    public void PastryCost_GetCorrectCostOfOnePastry_Int()
+    {
+      int testPastry = 1;
+      int testPastryCost = Pastry.PastryCost(testPastry);
+      Assert.AreEqual(2, testPastryCost);
+    }
+
+    [TestMethod]
+    public void PastryCost_GetCorrectCostOfMultiplePastry_Int()
     {
       int testPastry = 7;
       int testPastryCost = Pastry.PastryCost(testPastry);
@@ -23,7 +55,15 @@ namespace PierreBakery.Tests
     }
 
     [TestMethod]
-    public void FableCost_GetCorrectCostOfFableBreadStick_Int()
+    public void FableCost_GetCorrectCostOfOneFableBreadStick_Int()
+    {
+      int testFable = 1;
+      int testFableCost = Fable.FableCost(testFable);
+      Assert.AreEqual(100, testFableCost);
+    }
+
+    [TestMethod]
+    public void FableCost_GetCorrectCostOfMultipleFableBreadStick_Int()
     {
       int testFable = 15;
       int testFableCost = Fable.FableCost(testFable);
